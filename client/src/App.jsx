92bec619eddd533ef0197ddfbab255f577d97a91
SouhaRebai <souharebai111@gmail.com>
2023-04-12 22:44:34 +0100

    setup components + styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-
+import { Footer, Home, Loader, Navbar, Services, Transactions } from './components'
 import './App.css'
 
 const App = () => {
 
   return (
-    <div className="App">
-       <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div className='min-h-screen'>
+      <div className='gradient-bg-welcome'>
+        <Navbar/>
+        <Home/>
+      </div>
+      <Services/>
+      <Transactions/>
+      <Footer/>
   </div>
   )
 }
